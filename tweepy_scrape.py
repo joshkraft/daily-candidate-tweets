be068dd76de964041f2de70f3b98d6a401ebfe68
Josh Kraft <joshkraft757@gmail.com>
2020-10-18 11:59:54 -0400

    Printing filenames.

--- tweepy_scrape.py
+++ tweepy_scrape.py
@@ -1,6 +1,7 @@
 import tweepy
 import json
 import os
+import glob
 
 """
 
@@ -15,19 +16,23 @@ ACCESS_TOKEN_SECRET = secrets.ACCESS_TOKEN_SECRET
 
 # ACTIONS METHOD
 
+for root, dirs, files in os.walk("."):
+    for filename in files:
+        print(filename)
+        
 secret_file = open('secrets/secrets.json')
 secret_data = json.load(secret_file)
 print('Found secrets.')
 
-CONSUMER_KEY = os.getenv('INPUT_CONSUMER_KEY') # does this need quotes?
+"""CONSUMER_KEY = os.getenv('INPUT_CONSUMER_KEY') # does this need quotes?
 CONSUMER_SECRET = os.getenv('INPUT_CONSUMER_SECRET')
 ACCESS_TOKEN = os.getenv('INPUT_ACCESS_TOKEN')
-ACCESS_TOKEN_SECRET = os.getenv('INPUT_ACCESS_TOKEN_SECRET')
+ACCESS_TOKEN_SECRET = os.getenv('INPUT_ACCESS_TOKEN_SECRET')"""
 
-print(CONSUMER_KEY)
+"""print(CONSUMER_KEY)
 print(CONSUMER_SECRET)
 print(ACCESS_TOKEN)
-print(ACCESS_TOKEN_SECRET)
+print(ACCESS_TOKEN_SECRET)"""
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
