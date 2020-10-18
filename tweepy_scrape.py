186149cc0a93ec2c55303c30926c4db68384d67d
Josh Kraft <joshkraft757@gmail.com>
2020-10-17 20:15:54 -0400

    Trying tweepy for auth.

---
+++ tweepy_scrape.py
@@ -0,0 +1,29 @@
+import tweepy
+
+"""
+
+# LOCAL METHOD
+import secrets
+
+CONSUMER_KEY = secrets.CONSUMER_KEY
+CONSUMER_SECRET = secrets.CONSUMER_SECRET
+ACCESS_TOKEN = secrets.ACCESS_TOKEN
+ACCESS_TOKEN_SECRET = secrets.ACCESS_TOKEN_SECRET
+"""
+
+# ACTIONS METHOD
+
+CONSUMER_KEY = os.getenv(CONSUMER_KEY) # does this need quotes?
+CONSUMER_SECRET = os.getenv(CONSUMER_SECRET)
+ACCESS_TOKEN = os.getenv(ACCESS_TOKEN)
+ACCESS_TOKEN_SECRET = os.getenv(ACCESS_TOKEN_SECRET)
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+trump_tweets = api.user_timeline('realDonaldTrump', 
+                                 count = 10,
+                                 tweet_mode = 'extended')
+for tweet in trump_tweets:
+    print(tweet.created_at, tweet.full_text)
