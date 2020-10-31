bde068cc9b39a5c96ca262c738d5641c54232c53
Josh Kraft <joshkraft757@gmail.com>
2020-10-31 11:30:52 -0400

    Remobing str cast.

--- scrape.py
+++ scrape.py
@@ -40,7 +40,7 @@ def get_tweets_from_user(api, user):
                                    count = 200,
                                    include_rts = False,
                                    tweet_mode = 'extended')
-    updated_id = {user, str(tweets[-1].id)}
+    updated_id = {user, tweets[-1].id}
     last_retrieved_tweet_ids.update(updated_id)
     return tweets
 
