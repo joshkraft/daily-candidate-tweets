321a8fbe77b8f3095bd74efbca44f0f9abf7db2f
Josh Kraft <joshkraft757@gmail.com>
2020-11-03 07:31:23 -0500

    New approach... use tweet ids, no date partitioning.

--- scrape.py
+++ scrape.py
@@ -34,25 +34,23 @@ def get_last_tweet_ids():
         return json.load(file)
 
 
-def write_last_tweet_ids(most_recent_tweet_id_dict):
+def update_last_tweet_ids(last_tweet_ids):
     with open("most_recent_tweet_id.json", "w") as file:
-        json.dump(most_recent_tweet_id_dict, file)
+        json.dump(last_tweet_ids, file)
 
 
-def get_tweets_from_user(api, user, most_recent_tweet_id_dict):
-    if user in most_recent_tweet_id_dict:
+def get_tweets_from_user(api, user, last_tweet_ids):
+    if user in last_tweet_ids:
+        most_recent_tweet_id = int(last_tweet_ids[user])
         tweets = api.user_timeline(user, 
-                                   count = 200,
-                                   max_id = int(most_recent_tweet_id_dict[user]) - 1,
+                                   since_id = most_recent_tweet_id + 1,
                                    include_rts = False,
                                    tweet_mode = 'extended')
+    if tweets:
+        last_tweet_ids[user] =  str(tweets[0].id)
+        return tweets
     else:
-        tweets = api.user_timeline(user, 
-                                   count = 200,
-                                   include_rts = False,
-                                   tweet_mode = 'extended')
-    most_recent_tweet_id_dict[user] =  str(tweets[-1].id)
-    return tweets
+        print('No tweets pulled for ' + user + '. Check recent tweet id.')
 
 
 def upload_tweets(tweets, file_path):
@@ -60,37 +58,28 @@ def upload_tweets(tweets, file_path):
     return df.to_csv(file_path)
 
 
-def fromYesterday(tweet, yesterdays_date):
-    return tweet.created_at.strftime('%Y-%m-%d') == yesterdays_date
-
-
-def notRetweet(tweet):
-    return (tweet.retweeted == False) and ('RT @' not in tweet.full_text)
-
-
 def main():
     api = authenticate_with_secrets('/home/runner/secrets/secrets.json')
-    yesterdays_date = get_yesterdays_date()
     usernames = ["realDonaldTrump", "JoeBiden"]
 
-    most_recent_tweet_id_dict = get_last_tweet_ids()
+    last_tweet_ids = get_last_tweet_ids()
 
     for user in usernames:
-        file_path = "data/" + user + "/" + yesterdays_date + ".csv"
+        file_path = "data/" + user + "/data.csv"
         processed_tweets = []
-        for tweet in get_tweets_from_user(api, user, most_recent_tweet_id_dict):
-            tweet_details = {}
-            if notRetweet(tweet) and fromYesterday(tweet, yesterdays_date):
+        tweets = get_tweets_from_user(api, user, last_tweet_ids)
+        if tweets:
+            for tweet in tweets:
+                tweet_details = {}
+                tweet_details['id'] = tweet.id
                 tweet_details['username'] = tweet.user.screen_name
                 tweet_details['tweet_text'] = tweet.full_text
                 tweet_details['retweets'] = tweet.retweet_count
                 tweet_details['location'] = tweet.user.location
                 tweet_details['created_at'] = tweet.created_at
                 processed_tweets.append(tweet_details)
-
-        upload_tweets(processed_tweets, file_path)
-
-    write_last_tweet_ids(most_recent_tweet_id_dict)
+            upload_tweets(processed_tweets, file_path)
+    update_last_tweet_ids(last_tweet_ids)
 
 if __name__ == "__main__":
     main()
